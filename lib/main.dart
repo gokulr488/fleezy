d73eb3f877dbc585d2797431011e2776377de6d6
Gokul <gokulr488@gmail.com>
2021-02-20 18:56:42 +0530

    imports fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fleezy/Common/AppData.dart';
 import 'package:fleezy/Common/UiState.dart';
 import 'package:fleezy/screens/CreateCompanyScreen.dart';
 import 'package:fleezy/screens/HomeScreen.dart';
@@ -19,8 +20,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'Common/AppData.dart';
-
+//flutter build apk --split-per-abi
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
