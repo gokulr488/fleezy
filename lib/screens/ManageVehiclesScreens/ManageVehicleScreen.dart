f2bf395ae6d29e072c4dd2832682da36fbb97648
Gokul PC <gokulr488@gmail.com>
2021-02-27 19:58:02 +0530

    allowed drivers sheet added backend logic pending

--- lib/screens/ManageVehiclesScreens/ManageVehicleScreen.dart
+++ lib/screens/ManageVehiclesScreens/ManageVehicleScreen.dart
@@ -1,8 +1,13 @@
 import 'package:fleezy/Common/Alerts.dart';
 import 'package:fleezy/Common/AppData.dart';
+import 'package:fleezy/Common/UiConstants.dart';
+import 'package:fleezy/DataAccess/Roles.dart';
 import 'package:fleezy/DataAccess/Vehicle.dart';
+import 'package:fleezy/DataModels/ModelUser.dart';
 import 'package:fleezy/DataModels/ModelVehicle.dart';
 import 'package:fleezy/components/BaseScreen.dart';
+import 'package:fleezy/components/RoundedButton.dart';
+import 'package:fleezy/components/ScrollableList.dart';
 import 'package:fleezy/components/cards/ButtonCard.dart';
 import 'package:fleezy/components/cards/ManageVehicleCard.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +30,11 @@ class ManageVehicleScreen extends StatelessWidget {
               ButtonCard(
                   buttonText: 'Insurance Payment',
                   onTap: () => Navigator.pop(context)),
-              ButtonCard(buttonText: 'Allowed Drivers', onTap: () {}),
+              ButtonCard(
+                  buttonText: 'Allowed Drivers',
+                  onTap: () {
+                    allowerDrivers(context, vehicle);
+                  }),
               ButtonCard(
                   buttonText: 'Delete Vehicle',
                   onTap: () {
@@ -62,4 +71,57 @@ class ManageVehicleScreen extends StatelessWidget {
     Navigator.of(context).pop();
     return resp;
   }
+
+  void allowerDrivers(BuildContext context, ManageVehicleCard vehicle) async {
+    AppData appData = Provider.of<AppData>(context, listen: false);
+    if (appData.drivers == null) {
+      List<ModelUser> drivers =
+          await Roles().getAllUsersInCompany(appData.user.companyId);
+      appData.setDrivers(drivers);
+    }
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Container(
+              child: Column(
+            children: [
+              Expanded(
+                  child: ScrollableList(
+                      childrenHeight: 50,
+                      items: getDriverChoosers(appData.drivers, vehicle))),
+              RoundedButton(
+                  onPressed: saveAllowedDrivers,
+                  title: 'Save',
+                  colour: kHighlightColour,
+                  width: 200)
+            ],
+          ));
+        });
+  }
+
+  List<Widget> getDriverChoosers(
+      List<ModelUser> drivers, ManageVehicleCard vehicle) {
+    List<Widget> driverChoosers = [];
+    for (ModelUser driver in drivers) {
+      driverChoosers.add(Padding(
+        padding: const EdgeInsets.all(10.0),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            driver.fullName,
+            style: TextStyle(fontSize: 17),
+          ),
+          Checkbox(
+              activeColor: kHighlightColour,
+              value: true, // Need to find out if this driver has access already
+              onChanged: (bool value) {
+                vehicle.vehicle.allowedDrivers.add(driver.phoneNumber);
+              })
+        ]),
+      ));
+    }
+    return driverChoosers;
+  }
+
+  saveAllowedDrivers() {}
 }
