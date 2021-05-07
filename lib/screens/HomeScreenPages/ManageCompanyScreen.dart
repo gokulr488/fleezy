32cec9469c8cf18f2d5ac16ce809c22e8628e430
Gokul <gokulr488@gmail.com>
2021-05-07 19:37:33 +0530

    conditional UI for admin and non admin

--- lib/screens/HomeScreenPages/ManageCompanyScreen.dart
+++ lib/screens/HomeScreenPages/ManageCompanyScreen.dart
@@ -1,8 +1,11 @@
+import 'package:fleezy/Common/UiState.dart';
 import 'package:fleezy/components/GridLayout.dart';
+import 'package:fleezy/components/cards/BaseCard.dart';
 import 'package:fleezy/components/cards/ButtonCard.dart';
 import 'package:fleezy/screens/ManageDriverScreens/ManageDriversScreen.dart';
 import 'package:fleezy/screens/ManageVehiclesScreens/ManageVehiclesScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ManageCompanyScreen extends StatefulWidget {
   @override
@@ -12,22 +15,34 @@ class ManageCompanyScreen extends StatefulWidget {
 class _ManageCompanyScreenState extends State<ManageCompanyScreen> {
   @override
   Widget build(BuildContext context) {
-    return GridLayout(children: [
-      ButtonCard(
-          text: 'Manage Vehicles',
-          onTap: () {
-            Navigator.pushNamed(context, ManageVehiclesScreen.id);
-          }),
-      ButtonCard(
-          text: 'Manage Drivers',
-          onTap: () {
-            Navigator.pushNamed(context, ManageDriversScreen.id);
-          }),
-      ButtonCard(
-          text: 'Reports',
-          onTap: () {
-            // Navigator.pushNamed(context, ManageDriversScreen.id);
-          })
-    ]);
+    UiState uiState = Provider.of<UiState>(context, listen: false);
+    return uiState.isAdmin
+        ? GridLayout(children: [
+            ButtonCard(
+                text: 'Manage Vehicles',
+                onTap: () {
+                  Navigator.pushNamed(context, ManageVehiclesScreen.id);
+                }),
+            ButtonCard(
+                text: 'Manage Drivers',
+                onTap: () {
+                  Navigator.pushNamed(context, ManageDriversScreen.id);
+                }),
+            ButtonCard(
+                text: 'Reports',
+                onTap: () {
+                  // Navigator.pushNamed(context, ManageDriversScreen.id);
+                })
+          ])
+        : BaseCard(
+            cardChild: SizedBox(
+              height: 100,
+              child: Center(
+                  child: Text(
+                'Available for Company Administrators only',
+                style: TextStyle(fontSize: 17),
+              )),
+            ),
+          );
   }
 }
