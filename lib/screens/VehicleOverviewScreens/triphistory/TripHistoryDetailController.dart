fe777e42271169f942dc29e4b7ab6748db2751da
Gokul <gokulr488@gmail.com>
2021-05-10 19:26:22 +0530

    future builder for expense cards

--- lib/screens/VehicleOverviewScreens/triphistory/TripHistoryDetailController.dart
+++ lib/screens/VehicleOverviewScreens/triphistory/TripHistoryDetailController.dart
@@ -1,4 +1,3 @@
-
 import 'package:fleezy/Common/CallContext.dart';
 import 'package:fleezy/DataAccess/ExpenseApis.dart';
 import 'package:fleezy/DataModels/ModelExpense.dart';
@@ -6,22 +5,20 @@ import 'package:fleezy/DataModels/ModelTrip.dart';
 import 'package:fleezy/components/cards/ExpenseCard.dart';
 import 'package:flutter/material.dart';
 
-class TripHistoryDetailController {
-  List<ExpenseCard> expenseCards;
+Future<List<ModelExpense>> getExpenses(
+    ModelTrip tripDo, BuildContext context) async {
+  CallContext callContext =
+      await ExpenseApis().getExpensesInTrip(tripDo, context);
+  List<ModelExpense> expenses = callContext.data as List<ModelExpense>;
+  return expenses;
+}
 
-  void getExpenses(ModelTrip tripDo, BuildContext context) async {
-    CallContext callContext =
-        await ExpenseApis().getExpensesInTrip(tripDo, context);
-    List<ModelExpense> expenses = callContext.data as List<ModelExpense>;
-    _buildCards(expenses);
+List<ExpenseCard> buildCards(List<ModelExpense> expenses) {
+  List<ExpenseCard> expenseCards = [];
+  for (ModelExpense expense in expenses) {
+    expenseCards.add(ExpenseCard(
+      expense: expense,
+    ));
   }
-
-  void _buildCards(List<ModelExpense> expenses) {
-    expenseCards = [];
-    for (ModelExpense expense in expenses) {
-      expenseCards.add(ExpenseCard(
-        expense: expense,
-      ));
-    }
-  }
+  return expenseCards;
 }
--- lib/screens/VehicleOverviewScreens/triphistory/TripHistoryDetailsScreen.dart
+++ lib/screens/VehicleOverviewScreens/triphistory/TripHistoryDetailsScreen.dart
@@ -1,4 +1,3 @@
-import 'package:fleezy/Common/AppData.dart';
 import 'package:fleezy/Common/UiConstants.dart';
 import 'package:fleezy/DataModels/ModelTrip.dart';
 import 'package:fleezy/components/BaseScreen.dart';
@@ -8,23 +7,13 @@ import 'package:fleezy/components/ScrollableList.dart';
 import 'package:fleezy/components/cards/TripDetailCard.dart';
 import 'package:fleezy/screens/VehicleOverviewScreens/triphistory/TripHistoryDetailController.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class TripHistoryDetailsScreen extends StatefulWidget {
+class TripHistoryDetailsScreen extends StatelessWidget {
   static const String id = 'TripHistoryDetailsScreen';
-
-  @override
-  _TripHistoryDetailsScreenState createState() =>
-      _TripHistoryDetailsScreenState();
-}
-
-class _TripHistoryDetailsScreenState extends State<TripHistoryDetailsScreen> {
-  TripHistoryDetailController ctrl = TripHistoryDetailController();
 
   @override
   Widget build(BuildContext context) {
     ModelTrip tripDo = ModalRoute.of(context).settings.arguments as ModelTrip;
-    getExpenses(tripDo, context);
     return BaseScreen(
       headerText: 'Trip Summary',
       child: Column(
@@ -32,21 +21,21 @@ class _TripHistoryDetailsScreenState extends State<TripHistoryDetailsScreen> {
           TripDetailCard(tripDo: tripDo),
           HorLine(),
           Text('Expenses', style: TextStyle(fontSize: 18, color: kWhite80)),
-          Consumer<AppData>(builder: (context, misData, _) {
-            if (ctrl.expenseCards == null) return LoadingDots(size: 100);
-
-            return Expanded(
-              child:
-                  ScrollableList(childrenHeight: 150, items: ctrl.expenseCards),
-            );
-          })
+          FutureBuilder(
+            future: getExpenses(tripDo, context),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return Expanded(
+                  child: ScrollableList(
+                      childrenHeight: 150, items: buildCards(snapshot.data)),
+                );
+              } else {
+                return LoadingDots(size: 100);
+              }
+            },
+          ),
         ],
       ),
     );
-  }
-
-  void getExpenses(ModelTrip tripDo, BuildContext context) async {
-    await ctrl.getExpenses(tripDo, context);
-    setState(() {});
   }
 }
