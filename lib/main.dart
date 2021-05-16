a4a3dd7ab03390935e422c7169081e1b3c29d4af
Gokul <gokulr488@gmail.com>
2021-05-16 12:37:58 +0530

    launch config file readded

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-//flutter build apk --split-per-abi
+//flutter build  apk --target-platform android-arm
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
