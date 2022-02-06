26517968273191443ff06850c066cc32cf7534e8
Gokul <gokulr488@gmail.com>
2022-02-06 19:38:24 +0530

    android project upgraded to flutter updates

--- lib/Common/UiState.dart
+++ lib/Common/UiState.dart
@@ -1,14 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UiState extends ChangeNotifier {
   bool _isAdmin = true;
+  Brightness _themeMode = Brightness.dark;
+  Color _baseColor = Colors.blue;
 
   //GETTERS
   bool get isAdmin => _isAdmin;
+  Brightness get themeMode => _themeMode;
+  Color get baseColor => _baseColor;
 
 //SETTERS
   void setIsAdmin({bool isAdmin}) {
     _isAdmin = isAdmin;
     notifyListeners(); //uncomment this if admin switching is not happening
+  }
+
+  set baseColor(Color baseColor) {
+    _baseColor = baseColor;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    Brightness newTheme =
+        _themeMode == Brightness.dark ? Brightness.light : Brightness.dark;
+    _themeMode = newTheme;
+    notifyListeners();
   }
 }
